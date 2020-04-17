0c137385a2df2dbb6bdb40beebd47acf8b45bf60
kaylakantola <kkantola@trhc.com>
2020-04-17 19:22:50 -0400

    game of life

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -28293,12 +28293,195 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(n); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var initialLastRow = [{
+  alive: false
+}, {
+  alive: false
+}, {
+  alive: false
+}, {
+  alive: false
+}, {
+  alive: false
+}, {
+  alive: false
+}, {
+  alive: false
+}, {
+  alive: false
+}, {
+  alive: false
+}, {
+  alive: false
+}];
+var initialCurrentRow = [{
+  alive: true
+}, {
+  alive: true
+}, {
+  alive: false
+}, {
+  alive: true
+}, {
+  alive: true
+}, {
+  alive: true
+}, {
+  alive: false
+}, {
+  alive: false
+}, {
+  alive: true
+}, {
+  alive: true
+}];
+var maxRows = 9;
+
+var checkRules = function checkRules(family) {
+  var familyMembers = Object.keys(family);
+  var me = family.me;
+  var livingNeighbors = familyMembers.filter(function (f) {
+    var alive = family[f] ? family[f].alive : false;
+    var notMe = f !== "me";
+    return alive && notMe;
+  }).length;
+
+  if (me.alive) {
+    // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+    // Any live cell with two or three live neighbours lives on to the next generation.
+    // Any live cell with more than three live neighbours dies, as if by overpopulation.
+    return livingNeighbors >= 2 && livingNeighbors <= 3;
+  } else {
+    // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+    return livingNeighbors === 3;
+  }
+};
+
+var fillRow = function fillRow(_ref) {
+  var last = _ref.last,
+      current = _ref.current;
+  var nextRow = current.map(function (item, i) {
+    var family = {
+      left: current[i - 1],
+      me: current[i],
+      right: current[i + 1],
+      bottomLeft: last[i - 1],
+      bottom: last[i],
+      bottomRight: last[i + 1]
+    };
+    var alive = checkRules(family);
+    return {
+      alive: alive
+    };
+  });
+  return nextRow;
+};
 
 var GameBoard = function GameBoard() {
-  return /*#__PURE__*/_react.default.createElement("div", null, "GameBoard");
+  var _useState = (0, _react.useState)([]),
+      _useState2 = _slicedToArray(_useState, 2),
+      rows = _useState2[0],
+      setRows = _useState2[1];
+
+  var _useState3 = (0, _react.useState)(true),
+      _useState4 = _slicedToArray(_useState3, 2),
+      loading = _useState4[0],
+      setLoading = _useState4[1];
+
+  var play = function play() {
+    console.log("playing!");
+    var allRows = [];
+    var lastRow = initialLastRow;
+    var currentRow = initialCurrentRow;
+
+    var okGo = function okGo(_ref2) {
+      var last = _ref2.last,
+          current = _ref2.current;
+      console.log("go!");
+      var newRow = fillRow({
+        last: last,
+        current: current
+      });
+      allRows.push(lastRow);
+      lastRow = currentRow;
+      currentRow = newRow;
+
+      if (allRows.length - 1 < maxRows) {
+        okGo({
+          last: lastRow,
+          current: currentRow
+        });
+      } else {
+        var _rows = allRows.slice(1);
+
+        console.log({
+          rows: _rows
+        });
+        setLoading(false);
+        setRows(_rows);
+      }
+    };
+
+    okGo({
+      last: lastRow,
+      current: currentRow
+    });
+  };
+
+  (0, _react.useEffect)(function () {
+    play();
+  }, []); //eslint-disable-line
+
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("div", {
+    style: {
+      width: "1000px",
+      border: "1px solid black",
+      display: "flex",
+      flexDirection: "column"
+    }
+  }, loading && /*#__PURE__*/_react.default.createElement("div", null, "LOADING!"), !loading && rows.map(function (row) {
+    return /*#__PURE__*/_react.default.createElement(Row, {
+      row: row
+    });
+  })));
+};
+
+var Row = function Row(_ref3) {
+  var row = _ref3.row;
+  return /*#__PURE__*/_react.default.createElement("div", {
+    style: {
+      width: "100%",
+      display: "flex",
+      flexDirection: "row"
+    }
+  }, row.map(function (item) {
+    return /*#__PURE__*/_react.default.createElement("div", {
+      style: {
+        width: "10px",
+        height: "10px",
+        border: "1px solid red",
+        backgroundColor: item.alive ? "black" : "white"
+      }
+    });
+  }));
 };
 
 var _default = GameBoard;
@@ -28363,7 +28546,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62828" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51042" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
