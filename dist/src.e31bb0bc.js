97c52c29aaa32d466f995257d31a2439d4406f54
kaylakantola <kkantola@trhc.com>
2020-04-17 18:01:26 -0400

    gameboard

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -28285,21 +28285,57 @@ if ("development" === 'production') {
 } else {
   module.exports = require('./cjs/react-dom.development.js');
 }
-},{"./cjs/react-dom.development.js":"../node_modules/react-dom/cjs/react-dom.development.js"}],"index.js":[function(require,module,exports) {
+},{"./cjs/react-dom.development.js":"../node_modules/react-dom/cjs/react-dom.development.js"}],"gameboard/index.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var GameBoard = function GameBoard() {
+  return /*#__PURE__*/_react.default.createElement("div", null, "GameBoard");
+};
+
+var _default = GameBoard;
+exports.default = _default;
+},{"react":"../node_modules/react/index.js"}],"app.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _gameboard = _interopRequireDefault(require("./gameboard"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var App = function App() {
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h1", null, "Kayla's Game of Life"), /*#__PURE__*/_react.default.createElement(_gameboard.default, null));
+};
+
+var _default = App;
+exports.default = _default;
+},{"react":"../node_modules/react/index.js","./gameboard":"gameboard/index.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 var _react = _interopRequireDefault(require("react"));
 
 var _reactDom = _interopRequireDefault(require("react-dom"));
 
+var _app = _interopRequireDefault(require("./app.js"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var App = function App() {
-  return /*#__PURE__*/_react.default.createElement("div", null, "hohoho");
-};
-
-_reactDom.default.render( /*#__PURE__*/_react.default.createElement(App, null), document.getElementById("app"));
-},{"react":"../node_modules/react/index.js","react-dom":"../node_modules/react-dom/index.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+_reactDom.default.render( /*#__PURE__*/_react.default.createElement(_app.default, null), document.getElementById("app"));
+},{"react":"../node_modules/react/index.js","react-dom":"../node_modules/react-dom/index.js","./app.js":"app.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -28327,7 +28363,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62696" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62828" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import GameBoard from "./gameboard";
+const App = () => {
+  return (
+    <div>
+      <h1>Kayla's Game of Life</h1>
+      <GameBoard />
+    </div>
+  );
+};
+export default App;
