2d34fc59419be0ece6673174d94cc3dfbcdc15fa
kaylakantola <kkantola@trhc.com>
2020-04-17 17:39:39 -0400

    react

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>haha</div>;
+};
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App />, mountNode);
