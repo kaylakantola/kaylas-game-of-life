b6e261d154dd62359bb7076defe880ad0d18e58e
kaylakantola <kkantola@trhc.com>
2020-04-19 14:02:33 -0400

    play

--- src/gameboard/gameboard.js
+++ src/gameboard/gameboard.js
@@ -3,7 +3,32 @@ import { Row } from "../Row";
 import { Cell } from "../Cell";
 
 const GameBoard = ({ generations }) => {
-  return <div>Game Board</div>;
+  const [gen, setGen] = useState(0);
+
+  useEffect(() => {
+    if (gen - 1 === generations.length) {
+      setTimeout(() => setGen(gen + 1), 1000);
+    }
+  }, gen);
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {generations[gen].map((row, rowIdx) => (
+        <Row key={rowIdx}>
+          {row.map((cell, cellIdx) => (
+            <Cell key={cellIdx} cell={cell} />
+          ))}
+        </Row>
+      ))}
+    </div>
+  );
 };
 
 export default GameBoard;
